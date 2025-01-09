7ed9516e637ad36e66131bb5a8bb831a8d1c1f85
Alex Moore <166472371+mooreforlesss@users.noreply.github.com>
2025-01-09 11:23:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('Machine Learning App')
 
 st.write('This app builds a machine learning model')
 
-df = pd.read_csv('https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
