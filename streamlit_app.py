d7a3514e0fe3aaac0e7bdcce6f357e3648bae9eb
Alex Moore <166472371+mooreforlesss@users.noreply.github.com>
2025-01-09 12:02:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,8 @@ target_mapper = {'Adelie': 0,
 def target_encode(val):
   return target_mapper[val]
 
-  y = y_raw.apply(target_encode)
-  y
+y = y_raw.apply(target_encode)
+
 
 
 with st.expander('Data preparation'):
